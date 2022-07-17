3d26735c3c05552ebb1267133367c5a45648c8b6
Alex Kearns <alex@alexkearns.co.uk>
2022-07-17 11:33:36 +0100

    Process and log incoming stream events

--- stream_item_processor/app.py
+++ stream_item_processor/app.py
@@ -1,4 +1,8 @@
 import json
+from datetime import datetime, timezone
+
+
+PK_NAME = "PK"
 
 
 def lambda_handler(event, context):
@@ -16,5 +20,31 @@ def lambda_handler(event, context):
     None
     """
 
-    print(json.dumps(event, indent=2))
+    records = event["Records"]
+    for record in records:
+        event_type = record["eventName"]
+        pk_val = record["dynamodb"]["Keys"][PK_NAME]["S"]
+        approx_creation_datetime = datetime.fromtimestamp(
+            record["dynamodb"]["ApproximateCreationDateTime"],
+            timezone.utc
+        )
+
+        flat_new_image = {}
+
+        if event_type in ["INSERT", "MODIFY"]:
+            new_image = record["dynamodb"]["NewImage"]
+            for key, data in new_image.items():
+                flat_new_image[key] = list(data.values())[0]
+        elif event_type == "REMOVE":
+            flat_new_image[PK_NAME] = pk_val
+        else:
+            continue
+
+        flat_new_image["DynamoDbOperation"] = event_type
+        flat_new_image["DynamoDbOperationTime"] = approx_creation_datetime.strftime(
+            "%Y-%m-%d %H:%M:%S.%f"
+        )
+
+        print(json.dumps(flat_new_image))
+
     return None
